65d578faac03bb6b682a5a3978e9c4b17b90b773
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2018-05-28 16:32:59 -0700

    Updated Lesson

--- Section 3 - Program Flow Control in Python/ifprogramflow.py
+++ Section 3 - Program Flow Control in Python/ifprogramflow.py
@@ -18,23 +18,52 @@ __author__ = "Nhat Nguyen"
 #     print("Please come back in {0} years".format(18 - age))
 
 # Control Flow Example II
-guess = int(input("Please guess a number between 1 and 10: "))
+# else if (Java & JavaScript) is elif (Python)
+# Bad because repeated lines of code in the if and elif statements
+# guess = int(input("Please guess a number between 1 and 10: "))
 
-if guess < 5:
-    print("Please guess higher")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry, you have not guessed correctly.")
-elif guess > 5:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry, you have not guessed correctly.")
-else:
-    print("You got it first time")
+# if guess < 5:                                               # Enters when guess is greater than 5
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == 5:                                          # When guess is equal to 5
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly.")     # If did not enter to the previous conditions, enter
+# elif guess > 5:                                             # Enters when guess is less than 5
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly.")
+# else:
+#     print("You got it first time")                          # If not, (or guess is equal to 5) enter
 
 
+# Control Flow Example III (Better Version)
+# guess = int(input("Please guess a number between 1 and 10: "))
+#
+# if guess != 5:                                                # Enters when guess is NOT equal to 5
+#     if guess < 5:
+#         print("Please guess higher")
+#     else:
+#         print("Please guess lower.")
+#
+#     guess = int(input())
+#     if guess == 5:
+#         print("Sorry, you have not guessed correctly")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You got it first time")
+#
+# # Coding Exercise - Using if with strings
+# tree1 = 'put your first tree name here'
+# tree2 = 'put your second tree name here'
+#
+# if tree1 == tree2:
+#     print("The trees are the same")
+# else:
+#     print("The trees are different")
+
+# Lecture 31 - More Advanced If, Elif $ Else Processing
